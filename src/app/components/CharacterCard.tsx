7ce881c75381fbbbd339d920d99464fa2cb6e437
Raissa Reis Lopes <raissa.lopes@cloudwalk.io>
2025-02-23 03:22:36 -0300

    fix: adjust characteristics info

--- src/app/components/CharacterCard.tsx
+++ src/app/components/CharacterCard.tsx
@@ -15,23 +15,30 @@ const CharacterCard = ({ character }: { character: Character }) => {
 
   return (
     <div className="flex sm:block items-center h-[176px] sm:h-[478px] gap-x-[30px] hover:border hover:rounded-md hover:shadow-md bg-white">
-    {/* Imagem */}
     <img
       src={`https://picsum.photos/300/200?random=${character.name}`}
       alt={character.name}
       className="w-[115px] h-[130px] self-start sm:w-[432.5px] sm:h-[230px]"
     />
 
-    {/* Conteúdo no Mobile - Nome e Planeta apenas */}
     <div className="flex flex-col h-full justify-start sm:block ml-0 sm:ml-0">
-      <h2 className="text-[20px] sm:text-xl font-normal tracking-[1px] text-black">{character.name}</h2>
-      <p className="text-[15px] sm:text-base leading-7 tracking-[1px] font-normal text-black">{planetName}</p>
+      <div className=" h-7 mb-[5px] sm:mt-4">
+        <h2 className="text-[20px] sm:text-xl font-normal tracking-[1px] text-black">{character.name}</h2>
+      </div>
+      <div className="h-7 mb-[13px]">
+        <p className="text-[15px] sm:text-base leading-7 tracking-[1px] font-normal text-black">{planetName}</p>
+      </div>
 
-      {/* Detalhes exibidos apenas em telas maiores */}
       <div className="hidden sm:block mt-2">
-        <p className="text-sm text-gray-600">Height: {character.height}</p>
-        <p className="text-sm text-gray-600">Mass: {character.mass}</p>
-        <p className="text-sm text-gray-600">Gender: {character.gender}</p>
+      <div className="h-4 my-0">
+      <p className="text-[12px] leading-4 tracking-[0.5px] text-[#757575]">HEIGHT • {character.height}</p>
+      </div>
+      <div className="h-4 my-0">
+      <p className="text-[12px] leading-4 tracking-[0.5px] text-[#757575]">MASS • {character.mass}</p>
+      </div>
+      <div className="h-4 my-0">
+      <p className="text-[12px] leading-4 tracking-[0.5px] text-[#757575]">GENDER • {character.gender}</p>
+      </div>
       </div>
     </div>
   </div>
